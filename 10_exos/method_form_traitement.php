c984ed92e27b20161ac83027f972bb9e840b6c0e
Dhilip KARTHIKEYAN <74380649+K-Dhilip@users.noreply.github.com>
2022-06-13 11:50:27 +0200

    commit

--- 10_exos/method_form_traitement.php
+++ 10_exos/method_form_traitement.php
@@ -1,16 +1,17 @@
 <?php
     require_once('../inc/functions.php');
 
+    if(!empty($_POST)){
+        echo"<p>Prénom : " . $_POST['prenom'] . "</p>";
+        echo"<p>Nom : " . $_POST['nom'] . "</p>";
+        echo"<p>Commentaire : " . $_POST['commentaire'] . "</p>";
 
+        // on va écrire le contenu de la superglobale dans un fichier texteen l'absence de BDD
 
-    if (!empty($_POST)){
-        echo"<p> Prénom : " .$_POST['prenom']. "</p>";
-        echo"<p> Nom : " .$_POST['nom']. "</p>";
-        echo"<p> commentaire : " .$_POST['commentaire']. "</p>";
+        $fichier = fopen('formulaire.txt', 'a'); // fopenn() en mode 'a' permet de créer un fichier s'il n'existe pas encore, sinon cela permet de l'ouvrir
 
-        // on va écrire le contenu de la superglobale dans un fichier texte en l'absence de BDD
-        $fichier = fopen('formulaire.txt', 'a');// fopen() en mode "a" permet de créer un fichier s'il n'existe pas encore , sinon cela permet de l'ouvrir
-        $donneeformulaire = $_POST['prenom']. " " .$_POST['nom'] ." " . $_POST['commentaire']. "\n"; // \n pour faire des sauts de ligne dans le .txt
+        $donneeformulaire = $_POST['prenom']. " " .$_POST['nom']. " " .$_POST['commentaire'] . "\n"; // \n pour faier des retours à la ligne dans le .txt
+
         fwrite($fichier, $donneeformulaire);
     }
 ?>
